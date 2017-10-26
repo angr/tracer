fa3931d5107104d319e8963db4635faf59f35a00
Yan <yans@yancomm.net>
2017-10-25 17:09:28 -0700

    remove simprocedures from tracer setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='tracer', version='0.1', description="Symbolically trace concrete inputs.",
-    packages=['tracer', 'tracer.cachemanager', 'tracer.simprocedures'],
-    install_requires=[ 'shellphish-qemu' ],
+	name='tracer', version='0.1', description="Symbolically trace concrete inputs.",
+	packages=['tracer', 'tracer.cachemanager' ],
+	install_requires=[ 'shellphish-qemu' ],
 )
