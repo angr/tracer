e9c7c763eb0e0707aa232c29fdd6827c099aa1e7
Kyle ZENG <jkjh1jkjh1@gmail.com>
2019-01-15 17:56:59 -0800

    fix test set of tracer

--- tests/manual_cromu_recurse.py
+++ tests/manual_cromu_recurse.py
@@ -10,17 +10,16 @@ pov_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), "po
 test_data_location = str(os.path.dirname(os.path.realpath(__file__)))
 
 def test_recursion():
-    t = tracer.Tracer(os.path.join(bin_location, "cgc", "CROMU_00071"), open('crash2731').read())
+    t = tracer.Tracer(os.path.join(bin_location, "cgc", "CROMU_00071"), open('crash2731', 'rb').read())
 
     t.run()
 
 def run_all():
     functions = globals()
-    all_functions = dict(filter((lambda (k, v): k.startswith('test_')), functions.items()))
+    all_functions = dict(filter((lambda kv: kv[0].startswith('test_')), functions.items()))
     for f in sorted(all_functions.keys()):
         if hasattr(all_functions[f], '__call__'):
             all_functions[f]()
-
 
 if __name__ == "__main__":
     logging.getLogger("tracer").setLevel("DEBUG")
--- tracer/qemu_runner.py
+++ tracer/qemu_runner.py
@@ -39,7 +39,7 @@ class QEMURunner:
         """
         :param argv             : Optionally specify argv params (i,e,: ['./calc', 'parm1']).
         :param binary        : Path to the binary to be traced.
-        :param input         : Concrete input to feed to binary (string or CGC TracerPoV).
+        :param input         : Concrete input to feed to binary (bytes or CGC TracerPoV).
         :param project       : The original project.
         :param record_trace  : Whether or not to record the basic block trace.
         :param record_stdout : Whether ot not to record the output of tracing process.
