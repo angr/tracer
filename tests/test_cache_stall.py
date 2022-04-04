fae56129b994f7499ef4bd1d809af7476419afa4
mohitrpatil <mpatil14@asu.edu>
2022-04-04 15:34:19 -0700

    made changes in test_chall_stall.py as per feedback.

--- tests/test_cache_stall.py
+++ tests/test_cache_stall.py
@@ -1,8 +1,8 @@
 import os
 import tracer
+import unittest
 
 from angr.state_plugins.trace_additions import ZenPlugin
-from nose.plugins.attrib import attr
 
 import logging
 l = logging.getLogger("tracer.tests.test_cache_stall")
@@ -11,7 +11,7 @@ bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..
 pov_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), "povs"))
 test_data_location = str(os.path.dirname(os.path.realpath(__file__)))
 
-@attr(speed='slow')
+unittest.skip("broken")
 def broken_cache_stall():
     # test a valid palindrome
     t = tracer.Tracer(os.path.join(bin_location, "tests/cgc/CROMU_00071"), bytes.fromhex("0c0c492a53acacacacacacacacacacacacac000100800a0b690e0aef6503697d660a0059e20afc0a0a332f7d66660a0059e20afc0a0a332f7fffffff16fb1616162516161616161616166a7dffffff7b0e0a0a6603697d660a0059e21c"))
